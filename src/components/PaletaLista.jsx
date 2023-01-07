439ce3a35224bdf3a75b30e8d31aa13a6271021d
ruidantas <ruifisicauesb@hotmail.com>
2023-01-07 16:59:20 -0300

    add botão remover

--- src/components/PaletaLista.jsx
+++ src/components/PaletaLista.jsx
@@ -5,16 +5,28 @@ import "./PaletaLista.css";
 function PaletaLista() {
 
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
   const adicionarItem = (paletaIndex) => {
     const paleta = {[paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1}
     setPaletaSelecionada({...paletaSelecionada, ...paleta})
   }
 
+  const removerItem = (paletaIndex) => {
+    const paleta = {[paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) - 1}
+    setPaletaSelecionada({...paletaSelecionada, ...paleta})
+  }
+
+  const badgeCounter = (canRender, index) =>
+	Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletaSelecionada[index]} </span>);
+
+  const removeButton = (canRender, index) =>
+	Boolean(canRender) && (<button className='Acoes__remover' onClick={() => removerItem(index)}>remover</button>);
+
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) => (
         <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-          <span className='PaletaListaItem__badge'>{paletaSelecionada[index] || 0}</span>
+          {badgeCounter(paletaSelecionada[index], [index])}
           <div>
             <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
             <div className="PaletaListaItem__preco">
@@ -22,9 +34,10 @@ function PaletaLista() {
             </div>
             <div className="PaletaListaItem__descricao">{paleta.descricao}</div>
             <div className="PaletaListaItem__acoes Acoes">
-              <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={() => adicionarItem(index)}>
+              <button className={`Acoes__adicionar ${!paletaSelecionada[index] && "Acoes__adicionar--preencher"}`} onClick={() => adicionarItem(index)}>
                 adicionar
               </button>
+              {removeButton(paletaSelecionada[index], index)}
             </div>
           </div>
           <img
